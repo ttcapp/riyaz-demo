601136a392179f603109cc3cb1543937432add5e
afcking <owner@afcking.com>
2021-10-17 00:50:01 -0600

    17-10-21

---
+++ lib/my_home_page.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+String avatarSrc= "https://static.thenounproject.com/png/2416926-200.png";
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    double height=MediaQuery.of(context).size.height;
+    double width=MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.purple,
+        centerTitle: true,
+        title: Text("Riyaz Ahmed"),
+
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height:  width*0.25,
+                    width: width*0.25,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(avatarSrc),
+                        fit: BoxFit.fitHeight,
+                      ),
+                      border: Border.all(width: 3,
+                          color: Colors.deepOrange),
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(180)
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: width*0.025,
+                        ),
+                        Column(
+                          children: [
+                            Text("378", style:
+                            TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: width*0.055,
+                                color: Colors.black
+                               ),),
+                            Text("Posts"),
+                          ],
+                        ),
+                        SizedBox(
+                          width: width*0.025,
+                        ),
+                        Column(
+                          children: [
+                            Text("3.5K", style:
+                            TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: width*0.055,
+                                color: Colors.black
+                               ),),
+                            Text("Follower"),
+                          ],
+                        ),
+                        SizedBox(
+                          width: width*0.025,
+                        ),
+                        Column(
+                          children: [
+                            Text("100", style:
+                            TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: width*0.055,
+                                color: Colors.black
+                               ),),
+                            Text("Flowing"),
+                          ],
+                        ),
+
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 1,
+                                  color: Colors.grey,
+                              ),
+                            ),
+                           child: Padding(
+                             padding: const EdgeInsets.only(
+                               top: 4, left: 24, right: 24, bottom: 4
+                             ),
+                             child: Text("Message"),
+                           ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  width: 1,
+                                  color: Colors.grey,
+                              ),
+                            ),
+                           child: Padding(
+                             padding: const EdgeInsets.only(
+                               top: 4, left: 4, right: 4, bottom: 4
+                             ),
+                             child: Icon(Icons.account_circle, size: 16),
+                           ),
+                          ),
+                          ],
+                      ),
+                    ),
+
+                  ],
+                ),
+              ],
+            ),
+            ],
+
+        ),
+      ),
+
+    );
+  }
+}
